5a97ddd867380374235508b63ba9a0e112472db0
Владимир Мальцев <v.maltsev2@g.nsu.ru>
2024-12-13 14:34:31 +0700

    Изменил функции фильтрации по образцу bandpass, обернул файл в просто набор функций

--- spg/Calculate/Preprocessing/preproc.py
+++ spg/Calculate/Preprocessing/preproc.py
@@ -1,11 +1,33 @@
-class Preproc:
-    def __init__(self, data):
-        self.data = data
+from scipy import signal
 
-    def __repr__(self):
-        return str(self.data)
+def lin_detrend(data):
+    return signal.detrend(data, type='linear')
 
-    # Детренд - просто вставка из scipy.signal. Если необходимо расписать его более эффективно - можно перепрописать.
-    # Пока что нужен для простой проверки работа гитхаба. Пока даже закоментирую сам код.
-    def detrend(self):
-        return self.data # scipy.signal.detrend(self.data)
+def const_detrend(data):
+    return signal.detrend(data, type='constant')
+
+def deconvolution(data):
+    return data
+
+def butter_lowpass(lowcut, fs, order=5):
+    return signal.butter(order, lowcut, fs=fs, btype='low')
+def butter_lowpass_filter(data, lowcut, fs, order=5):
+    b, a = butter_lowpass(lowcut, fs, order=order)
+    filtered_data = signal.lfilter(b, a, data)
+    return filtered_data
+
+
+def butter_highpass(highcut, fs, order=5):
+    return signal.butter(order, highcut, fs=fs, btype='high')
+def butter_highpass_filter(data, highcut, fs, order=5):
+    b, a = butter_highpass(highcut, fs, order=order)
+    filtered_data = signal.lfilter(b, a, data)
+    return filtered_data
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    return signal.butter(order, [lowcut, highcut], fs=fs, btype='band')
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    filtered_data = signal.lfilter(b, a, data)
+    return filtered_data
