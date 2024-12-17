fc786d2b794514c8c2036249981472e669aee401
Владимир Мальцев <v.maltsev2@g.nsu.ru>
2024-12-17 10:42:19 +0700

    Изменил файл __init__.py внутри папки Calculate

--- spg/Calculate/__init__.py
+++ spg/Calculate/__init__.py
@@ -1,1 +1,1 @@
-from .Preprocessing import Preproc
+from .Preprocessing import lin_detrend, const_detrend, deconvolution, butter_lowpass, butter_lowpass_filter, butter_highpass, butter_highpass_filter, butter_bandpass, butter_bandpass_filter
